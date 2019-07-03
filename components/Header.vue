921908e55813104fbefa46d89d0417a602fbe195
DevIsho <ishmaelkargbo5@gmail.com>
2019-07-03 15:05:54 +0000

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="px-24 shadow-lg">
+    <div class="flex items-center py-5 justify-between">
+      <div>
+        <img src="~/assets/images/dologo.png" class="h-10 w-40 cursor-pointer" />
+      </div>
+      <div class="flex text-sm">
+        <div class="mx-2">
+          <nuxt-link to>Home</nuxt-link>
+        </div>
+        <div class="mx-2">
+          <nuxt-link to>About Us</nuxt-link>
+        </div>
+        <div class="mx-2">
+          <nuxt-link to>Services</nuxt-link>
+        </div>
+        <div class="mx-2">
+          <nuxt-link to>Portfolio</nuxt-link>
+        </div>
+        <div class="mx-2">
+          <nuxt-link to>Contact Us</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+      landing
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
