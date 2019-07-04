4bced53e084613edbf1133bc742fbfde013e75c6
DevIsho <ishmaelkargbo5@gmail.com>
2019-07-04 10:42:58 +0000

    about

--- components/Header.vue
+++ components/Header.vue
@@ -4,9 +4,9 @@
       <div>
         <img src="~/assets/images/dologo.png" class="h-16 w-48 cursor-pointer" />
       </div>
-      <div class="flex text-sm">
+      <div class="flex text-sm font-semibold">
         <div class="mx-2">
-          <nuxt-link to>Home</nuxt-link>
+          <nuxt-link to="/">Home</nuxt-link>
         </div>
         <div class="mx-2">
           <nuxt-link to>About Us</nuxt-link>
--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="pt-40 mx-24 pb-10">
-    <p class="text-3xl font-semibold">Portfolio </p>
+  <div class="pt-40 mx-24 pb-24">
+    <p class="text-3xl font-semibold border-b pb-5">Portfolio</p>
     <div class="flex mt-10">
-      <div class="w-1/2 relative img cursor-pointer">
-        <img class="w-full px-2 h-75" src="~/assets/images/IMG-20190622-WA0027.jpg" alt />
-        <div class="w-full top-0 h-75 absolute">
+      <div class="w-1/3 relative img cursor-pointer">
+        <img class="w-full px-2 h-64" src="~/assets/images/IMG-20190622-WA0027.jpg" alt />
+        <div class="w-full top-0 h-64 absolute">
           <div class="pup-over h-full mx-2 hidden">
             <div class="flex h-full text-white items-center justify-center">
               <div class="font-semibold text-center">
-                <p><p class="bg-black w-12 py-2 text-center rounded"><i class="el-icon-plus"></i></p></p>
+                <p class="bg-black w-8 py-1 text-center rounded-full">
+                  <i class="el-icon-plus"></i>
+                </p>
                 <p class="text-base mt-2">Name</p>
                 <p class="text-gray-800 uppercase text-sm">type</p>
               </div>
@@ -16,13 +18,31 @@
           </div>
         </div>
       </div>
-      <div class="w-1/2 relative img cursor-pointer">
-        <img class="w-full px-2 h-75" src="~/assets/images/IMG-20190622-WA0029.jpg" alt />
-        <div class="w-full top-0 h-75 absolute">
+      <div class="w-1/3 relative img cursor-pointer">
+        <img class="w-full px-2 h-64" src="~/assets/images/IMG-20190622-WA0028.jpg" alt />
+        <div class="w-full top-0 h-64 absolute">
           <div class="pup-over h-full mx-2 hidden">
             <div class="flex h-full text-white items-center justify-center">
               <div class="font-semibold text-center">
-                <p><p class="bg-black w-12 py-2 text-center rounded"><i class="el-icon-plus"></i></p></p>
+                <p class="bg-black w-8 py-1 text-center rounded-full">
+                  <i class="el-icon-plus"></i>
+                </p>
+                <p class="text-base mt-2">Name</p>
+                <p class="text-gray-800 uppercase text-sm">type</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/3 relative img cursor-pointer">
+        <img class="w-full px-2 h-64" src="~/assets/images/IMG-20190622-WA0029.jpg" alt />
+        <div class="w-full top-0 h-64 absolute">
+          <div class="pup-over h-full mx-2 hidden">
+            <div class="flex h-full text-white items-center justify-center">
+              <div class="font-semibold text-center">
+                <p class="bg-black w-8 py-1 text-center rounded-full">
+                  <i class="el-icon-plus"></i>
+                </p>
                 <p class="text-base mt-2">Name</p>
                 <p class="text-gray-800 uppercase text-sm">type</p>
               </div>
