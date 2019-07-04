180ed84bad8f328e91835da2103fc6ddd894bb82
alhassan84 <turayalhassan123@gmail.com>
2019-07-03 22:40:09 -1200

    portfolio

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -2,7 +2,7 @@
   <div class="pt-40 mx-24 pb-10">
     <p class="text-3xl font-semibold">Portfolio </p>
     <div class="flex mt-10">
-      <div class="w-1/2 relative img cursor-pointer">
+      <div class="w-1/2 relative img cursor-pointer" @click="viewPortfolio(1)">
         <img class="w-full px-2 h-75" src="~/assets/images/IMG-20190622-WA0027.jpg" alt />
         <div class="w-full top-0 h-75 absolute">
           <div class="pup-over h-full mx-2 hidden">
@@ -34,6 +34,12 @@
   </div>
 </template>
 <script>
-export default {};
+export default {
+    methods: {
+        viewPortfolio(index){
+            this.$router.push('/portfolio/view_portfolio')
+        }
+    }
+};
 </script>
 
